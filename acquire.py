08dff38cb69f07df0693a5137e30ab237a0ba3f3
David Mitchell <Davidlee.Mitchell2@Gmail.com>
2022-06-06 11:05:47 -0500

    acquire.py

---
+++ acquire.py
@@ -0,0 +1,55 @@
+import pandas as pd
+import numpy as np
+import os
+from env import host, username, password
+import warnings
+warnings.filterwarnings("ignore")
+
+
+# ****************************  connection **********************************************
+
+# Create helper function to get the necessary connection url.
+def get_connection(db_name):
+    '''
+    This function uses my info from my env file to
+    create a connection url to access the Codeup db.
+    '''
+    from env import host, username, password
+    return f'mysql+pymysql://{username}:{password}@{host}/{db_name}'
+
+
+# **************************** Zillow ******************************************************
+
+ 
+#acquire data for the first time
+def get_new_zillow():
+    '''
+    This function reads in the zillow data from the Codeup db
+    and returns a pandas DataFrame with columns :
+     bedroomcnt, bathroomcnt, calculatedfinishedsquarefeet, taxvaluedollarcnt, yearbuilt, taxamount, fips 
+    '''
+    sql_query = '''
+    SELECT bedroomcnt, bathroomcnt, calculatedfinishedsquarefeet, taxvaluedollarcnt, yearbuilt, taxamount, fips
+    FROM properties_2017
+    WHERE propertylandusetypeid = 261
+    '''
+    return pd.read_sql(sql_query, get_connection('zillow'))
+
+#acquire data main function 
+def get_zillow():
+    '''
+    This function reads in zillow data from Codeup database, writes data to
+    a csv file if a local file does not exist, and returns a df.
+    '''
+    if os.path.isfile('zillow.csv'):
+        
+        # If csv file exists, read in data from csv file.
+        df = pd.read_csv('zillow.csv', index_col=0)
+        
+    else:
+        
+        # Read fresh data from db into a DataFrame.
+        df = get_new_zillow()
+        
+        # Write DataFrame to a csv file.
+        df.to_csv('zillow.csv')
